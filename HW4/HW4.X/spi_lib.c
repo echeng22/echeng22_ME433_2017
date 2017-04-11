548d877745a408b5ff2dfe3c0650f9302bd1f8cf
Elton Cheng <elton.c22@gmail.com>
2017-04-10 19:21:14 -0500

    HW4 code

---
+++ HW4/HW4.X/spi_lib.c
@@ -0,0 +1,20 @@
+#include <proc/p32mx250f128b.h>
+
+#include "spi_lib.h"
+
+void setVoltage(char channel, char voltage)
+{
+    if(channel == 'a')
+    {
+        
+    }
+    else if (channel == 'b')
+    {
+        
+    }
+}
+
+void initSPI1()
+{
+    
+}
---
+++ HW4/HW4.X/spi_lib.h
@@ -0,0 +1,7 @@
+#ifndef SPI_LIB_H_
+#define SPI_LIB_H_
+
+void initSPI1();
+void setVoltage(char channel, char voltage);
+
+#endif
